6854687598a38cb6c65725192895b70b722c4f74
Alex Pritchard <alexbeet1234@gmail.com>
2021-10-09 19:17:12 +0800

    added axios

--- app/javascript/components/GameList/GameList.js
+++ app/javascript/components/GameList/GameList.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './GameList.css';
 
 const GameList = () => {
+  // call api and get a list of games.
+  // use axios to do call.
+  
+  // useState and save result into an array
+
+  // map over array and create a div for each entry
+
+  // place div into return statement.
+
   return (
     <div>
       <div className="search_bar">
