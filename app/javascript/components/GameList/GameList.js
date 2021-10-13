4b1078daeac4670dfa84dfe0cd9eca6cebff97cb
Alex Pritchard <alexbeet1234@gmail.com>
2021-10-13 10:37:49 +0800

    updated seeds file

--- app/javascript/components/GameList/GameList.js
+++ app/javascript/components/GameList/GameList.js
@@ -6,35 +6,40 @@ const GameList = () => {
   // call api and get a list of games.
   // use axios to do call.
 
-  // const searchGameHandler = (event) => {
-  //   event.preventDefault();
-  //   console.log(event);
-  //   axios({
-  //     url: "https://api.igdb.com/v4/games",
-  //     method: 'POST',
-  //     headers: {
-  //         'Accept': 'application/json',
-  //         'Client-ID': 'kbfk11t8q6lb33clzkf074fflfrk2b',
-  //         'Authorization': 'Bearer 0r29zp7b8uwswd6rjed08svor0h0jf',
-  //     },
-  //     data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
-  //   })
-  //     .then(response => {
-  //         console.log(response.data);
-  //     })
-  //     .catch(err => {
-  //         console.error(err);
-  //     });
-  // }
-
   const searchGameHandler = (event) => {
     event.preventDefault();
-    axios(`https://www.giantbomb.com/api/games/?api_key=af926dff67c16cd52bc4feffa6372f7b760ca93a&format=json`, {
-
+    console.log(event);
+    axios({
+      url: "https://api.igdb.com/v4/games",
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Client-ID': 'kbfk11t8q6lb33clzkf074fflfrk2b',
+          'Authorization': 'Bearer 0r29zp7b8uwswd6rjed08svor0h0jf',
+      },
+      data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
     })
-      .then(response => console.log(response))
-      .catch(response => console.log(response))    
-  };
+      .then(response => {
+          console.log(response.data);
+      })
+      .catch(err => {
+          console.error(err);
+      });
+  }
+
+  if (typeof console === 'object') {
+    console.log('// To test a local CORS Anywhere server, set cors_api_url. For example:');
+    console.log('cors_api_url = "http://localhost:3000/"');
+  }
+
+  // const searchGameHandler = (event) => {
+  //   event.preventDefault();
+  //   axios(`https://www.giantbomb.com/api/games/?api_key=af926dff67c16cd52bc4feffa6372f7b760ca93a&format=json`, {
+
+  //   })
+  //     .then(response => console.log(response))
+  //     .catch(response => console.log(response))    
+  // };
 
   
 
