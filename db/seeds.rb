b9111f9089dbab912bea12a80517d2fb4caa264d
Alex Pritchard <alexbeet1234@gmail.com>
2021-10-26 09:58:42 +0800

    updated GameList.js to call the api

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,15 @@ require 'json'
 require 'open-uri'
 require 'net/https'
 
-search = "zelda"
+# search = "zelda"
 
-http = Net::HTTP.new('api.igdb.com',443)
-http.use_ssl = true
-request = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => 'kbfk11t8q6lb33clzkf074fflfrk2b', 'Authorization' => 'Bearer 0r29zp7b8uwswd6rjed08svor0h0jf'})
-# request.body = 'fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;'
-# request.body = 'fields name,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,storyline,summary,total_rating;'
-request.body = 'fields name,platforms,player_perspectives,rating,rating_count,total_rating,release_dates,screenshots,storyline; search "' + search + '"; limit 500;'
-puts games = JSON.parse(http.request(request).body)
+# http = Net::HTTP.new('api.igdb.com',443)
+# http.use_ssl = true
+# request = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => 'kbfk11t8q6lb33clzkf074fflfrk2b', 'Authorization' => 'Bearer 0r29zp7b8uwswd6rjed08svor0h0jf'})
+# # request.body = 'fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;'
+# # request.body = 'fields name,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,storyline,summary,total_rating;'
+# request.body = 'fields name,platforms,player_perspectives,rating,rating_count,total_rating,release_dates,screenshots,storyline; search "' + search + '"; limit 100;'
+# puts games = JSON.parse(http.request(request).body)
 # count = 1
 # games.map do |item|
 #   puts item["name"]
@@ -41,19 +41,19 @@ puts games = JSON.parse(http.request(request).body)
 #   # end
 # end
 
-# url = "https://api.igdb.com/v4/games"
+url = "localhost:3030/https://api.igdb.com/v4/games"
 
-# games_serialized = URI.open(url, {
-#   "Client-ID" => "kbfk11t8q6lb33clzkf074fflfrk2b",
-#   "Authorization" => "Bearer 0r29zp7b8uwswd6rjed08svor0h0jf",
-#   # fields *;
-# }).read
+games_serialized = URI.open(url, {
+  "Client-ID" => "kbfk11t8q6lb33clzkf074fflfrk2b",
+  "Authorization" => "Bearer 0r29zp7b8uwswd6rjed08svor0h0jf",
+  # fields *;
+}).read
 
-# games = JSON.parse(games_serialized)
+games = JSON.parse(games_serialized)
 
-# games.map do |game|
-#   puts game
-# end
+games.map do |game|
+  puts game
+end
 
 ## -- GIANT BOMB API --
 ## seach one game at a time
